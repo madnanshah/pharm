d5df37a3122ee549964f402cf36870eb7606e3b8
makhan00 <madnanshah@users.noreply.github.com>
2022-03-09 01:15:44 +0100

    06-03-22-01 : Some Structure change in layouts

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -8,6 +8,6 @@ use Illuminate\Http\Request;
 class AdminController extends Controller
 {
     public function dashboard(){
-        return view('admin.dashboard');
+        return view('layouts.dashboard');
     }
 }
